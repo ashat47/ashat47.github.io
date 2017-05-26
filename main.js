3150fa0f26991921965df66a9435bfeaf207e887
ashat47 <ashat47@mail.ru>
2017-05-26 20:05:05 +0300

    мой первый коммит

---
+++ main.js
@@ -0,0 +1,20 @@
+var value = [123123, 123123, 12313] ;
+var result=25;
+var text='asdasdasdasdasdasdadas \n asdadasd'
+result=10/5;
+
+console.log (result);
+
+//обьекты для взаимодействия с браузером и их методами
+/*
+  блаблаблабла
+  блаблаблабла
+*/
+
+
+document.write(text+'</br>');
+document.write(result);
+console.log(document.getElementById('1'));
+
+
+ undefined
